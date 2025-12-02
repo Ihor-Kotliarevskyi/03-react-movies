597fd97a743e2767696b65496e4ada9038d672e8
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-12-02 22:17:33 +0200

    add markup and styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,1 +1,21 @@
+import { useState } from "react";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import Loader from "../Loader/Loader";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import SearchBar from "../SearchBar/SearchBar";
 
+function App() {
+  const [isHide, setIsHide] = useState(true);
+  return (
+    <>
+      <SearchBar />
+      {!isHide && <MovieGrid />}
+      {!isHide && <Loader />}
+      {!isHide && <ErrorMessage />}
+      {!isHide && <MovieModal />}
+    </>
+  );
+}
+
+export default App;
